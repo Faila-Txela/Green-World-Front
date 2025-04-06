63038a2634d1cbf1b5eedc1060ac131fd31ad6b9
Faila Txela <129459141+Faila-Txela@users.noreply.github.com>
2025-04-06 09:20:03 +0100

    more

--- src/modules/service/api/provincia/index.ts
+++ src/modules/service/api/provincia/index.ts
@@ -5,5 +5,20 @@ class ProvinciaService{
         const { data } = await axios.get("/provincia");
         return data;
      }
+
+     async create(data: { nome: string }){
+      const response = await axios.post("/provincia", data);
+      return response.data;
+     }
+
+     async update(id: string, data: { nome: string }){
+      const response = await axios.put(`/provincia/${id}`, data);
+      return response.data;
+     }
+     
+     async delete(id: string){
+      const response = await axios.delete(`/provincia/${id}`);
+      return response.data;
+     }
 }
 export const provinciaService = new ProvinciaService();
