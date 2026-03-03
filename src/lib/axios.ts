792691ec70c3b782e973e6378ba590d34f3b871e
Faila Txela <129459141+Faila-Txela@users.noreply.github.com>
2026-03-03 21:29:44 +0100

    lib axios changed

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export default axios.create(
     {
-        baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3003',
+        baseURL: import.meta.env.VITE_API_URL,
         withCredentials: true,
         //baseURL: 'http://127.0.0.1:3003'
+        // || 'http://localhost:3003'
     }
 )
